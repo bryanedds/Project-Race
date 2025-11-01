d3e6e8680f575c4a911b528c5ab7b9a5b47ac728
bryanedds <bryanedds@gmail.com>
2025-11-01 18:05:47 -0400

    Added build scripts.

---
+++ Projects/Project Race/Windows.x64.Clean.bat
@@ -0,0 +1,4 @@
+dotnet clean -c Release "../../Nu/Nu.Pipe/Nu.Pipe.fsproj"
+rmdir /s /q ".\bin\Release\net9.0\win-x64"
+rmdir /s /q ".\bin\Release\net9.0\Assets"
+pause
---
+++ Projects/Project Race/Windows.x64.Publish.bat
@@ -0,0 +1,11 @@
+dotnet build -c Release "../../Nu/Nu.Pipe/Nu.Pipe.fsproj"
+dotnet publish -c Release -r win-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained true "Project Race.fsproj"
+cd ".\bin\Release\net9.0\win-x64"
+rename "assimp.dll" "assimp.dll.tmp" & REM for some reason, assimp.dll doesn't get folded into the .exe image, so we preserve it.
+del *.dll
+del *.pdb
+del *.so
+del *.xml
+rename "assimp.dll.tmp" "assimp.dll"
+xcopy "..\Assets" ".\Assets" /E /I /Y
+pause
